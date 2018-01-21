2f45345eafbab553d72bd3a38f35b53c4dbca72b
Vishesh <visheshsingh16@gmail.com>
2018-01-21 11:52:31 -0600

    function for click on img

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 const current = document.querySelector("#current");
 const imgs = document.querySelectorAll(".imgs img");
 
-imgs.forEach(img => img.addEventListener('click', e => current.src = e.target.src));
+imgs.forEach(img => img.addEventListener('click', imgClick));
+
+function imgClick(e){
+    current.src = e.target.src;
+}
