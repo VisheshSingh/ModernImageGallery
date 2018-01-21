c94d7051be6ace1189f9334c6ccac50cc8a142b9
Vishesh <visheshsingh16@gmail.com>
2018-01-21 11:56:17 -0600

    Reset opacity

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 const current = document.querySelector("#current");
 const imgs = document.querySelectorAll(".imgs img");
-
+const opacity = 0.5;
 imgs.forEach(img => img.addEventListener('click', imgClick));
 
 function imgClick(e){
+    //Reset opacity
+    imgs.forEach(img => (img.style.opacity = 1));
+    //Chnage current image to src of click image
     current.src = e.target.src;
+    //change opacity to opacity variable
+    e.target.style.opacity = opacity;
 }
